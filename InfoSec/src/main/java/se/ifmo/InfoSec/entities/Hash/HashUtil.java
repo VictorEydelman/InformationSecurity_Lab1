c292844e60b32c87baaa10cde1cd3cc24175da8b
VictorEydelman <113546427+VictorEydelman@users.noreply.github.com>
2025-09-13 09:07:46 +0300

    fix pom

--- InfoSec/src/main/java/se/ifmo/InfoSec/entities/Hash/HashUtil.java
+++ InfoSec/src/main/java/se/ifmo/InfoSec/entities/Hash/HashUtil.java
@@ -7,15 +7,6 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class HashUtil {
-    public static String digectPassword(String password) throws NoSuchAlgorithmException {
-        MessageDigest messageDigest = MessageDigest.getInstance("MD5");
-        messageDigest.update(password.getBytes(),0,password.length());
-        String hashedPass = new BigInteger(1,messageDigest.digest()).toString(16);
-        if(hashedPass.length()<32){
-            hashedPass="0"+hashedPass;
-        }
-        return hashedPass;
-    }
     public static String hashPassword(String password) {
         return BCrypt.hashpw(password, BCrypt.gensalt());
     }
